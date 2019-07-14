fefc7f9056bb253f63edc42fade8a782d5b8d915
joeduffy <joe@pulumi.com>
2019-07-13 17:32:41 -0700

    Run tests as part of preview/updates
    These tests depend on two things that aren't available in
    PULUMI_TEST_MODE:
    
        1) Resource functions (like aws.getRegion()).
    
        2) Resource outputs (like the EKS cluster's version).
    
    Instead of running without the Pulumi CLI in the picture --
    which is necessary for (1) and (2) to work -- we can instead
    run the tests from within the Pulumi program itself.
    
    This has the extra benefit that tests will be run as part of
    any update, and they are properly orchestrated with respect
    to the engine's activities around previews and updates.
    
    To run the tests, simply run
    
        $ pulumi preview
    
    or just do an update as usual
    
        $ pulumi up
    
    and you will see the test output emitted to the console.
    
    We plan on reconciling this with PULUMI_TEST_MODE, and removing
    the need to use the promise<T> function in here, as part of
    https://github.com/pulumi/pulumi/issues/2287.

--- test/test.ts
+++ test/test.ts
@@ -1,33 +1,10 @@
-
-process.env.PULUMI_TEST_MODE = 'true';
-process.env.PULUMI_NODEJS_STACK = 'stack-name';
-process.env.PULUMI_NODEJS_PROJECT = 'infra-eks';
-
-import * as yaml from 'js-yaml';
 import { expect } from 'chai';
-import * as pulumi from "@pulumi/pulumi";
-import 'mocha';
-
-const pulumiConfig = yaml.safeLoad(require('fs').readFileSync('./Pulumi.unit-test.yaml', 'utf8'));
-process.env.PULUMI_CONFIG = JSON.stringify(pulumiConfig.config);
-
-import * as index from '../index';
+import { promise } from './';
+import { cluster } from '../';
 
 describe('#EKS Cluster Deployment', () => {
-    let cluster = index.cluster;
-    it('Should Deploy EKS Cluster', (done) => {
-        cluster.eksCluster.version.apply(([version]) => asyncDone(() => {
-            expect(version).to.equals('should fail');
-        }, done));
+    it('Should Deploy EKS Cluster', async () => {
+        const version = await promise(cluster.eksCluster.version);
+        expect(version).to.equals("should fail");
     });
 });
-
-const asyncDone = (testFn: Function, done: Function) => {
-    try {
-        testFn();
-
-        done();
-    } catch (err) {
-        done(err);
-    }
-}
