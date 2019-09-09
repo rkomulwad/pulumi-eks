216403de4329e7e3c92af60eb9fca2372484257f
rkomulwad <D4KEXyRPjpo>
2019-09-09 16:09:23 -0400

    updated to latest version of libs

--- index.ts
+++ index.ts
@@ -4,10 +4,11 @@ import * as eks from "@pulumi/eks";
 const name = "helloworld";
 
 // Create an EKS cluster with non-default configuration
-const vpc = new awsx.ec2.Vpc("vpc", { subnets: [{ type: "public" }] });
+const vpc = new awsx.ec2.Vpc("vpc");
 export const cluster = new eks.Cluster(name, {
         vpcId: vpc.id,
         subnetIds: vpc.publicSubnetIds,
+        instanceType: "t2.micro",
         desiredCapacity: 1,
         minSize: 1,
         maxSize: 2,
